4561d99d8b8912bf21f36e0f2a43b59f90c03880
Anna Wilson <awilson50@smith.edu>
2022-11-03 11:49:29 -0400

    create table fixed

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,4 +1,8 @@
-CREATE TABLE practice (
-    Test1 VARCHAR(20) 
-)
-
+CREATE TABLE books(
+	DateRead CHAR(6),
+	Author VARCHAR(100),
+	Title VARCHAR(100),
+	YearPublished VARCHAR(6),
+	Pages INT,
+	Favorite INT
+);
